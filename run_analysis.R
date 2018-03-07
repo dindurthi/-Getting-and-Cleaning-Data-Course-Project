549c3df21b7c40f57396046e4766a5b2348d00fb
dindurthi <34685801+dindurthi@users.noreply.github.com>
2018-03-07 12:12:26 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,10 @@
 library(reshape2)
-
-
-
 ## Download and unzip the dataset:
 
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
 download.file(fileURL,"./data/UCI HAR Dataset.zip")
-
-
 unzip("./data/getdata_dataset.zip") 
+
 ## Reading the dataset from the unzipped file
 
 # Read the train Dataset
